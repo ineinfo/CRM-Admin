0e024ee522693fff8ad5f225e09066b308638b94
ineinfotechservices <nikhil.ineinfotech@gmail.com>
2024-12-24 13:56:17 +0530

    config of next

--- next.config.js
+++ next.config.js
@@ -12,10 +12,21 @@ module.exports = {
     },
   },
   webpack(config) {
+    // Handling SVG imports using @svgr/webpack
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
+    // Additional Webpack optimizations (optional, depending on your project)
+    config.resolve.fallback = {
+      fs: false, // If you're using file system in the client-side, add this to avoid errors
+      path: false,
+      os: false,
+    };
+
     return config;
   },
+
+
 };
